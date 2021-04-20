8aa3c60e01aac47f8b7bd17bbabfce8440122bfb
rita-mordio <ritamordio0716@gmail.com>
2021-04-20 14:26:40 +0900

    - SignIn 페이지 자동로그인 퍼블리싱 및 state 연결

--- src/pages/SignIn.js
+++ src/pages/SignIn.js
@@ -1,17 +1,50 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Button, Form, Grid, Segment } from 'semantic-ui-react';
 
 const SignIn = () => {
+  const [userInfo, setUserInfo] = useState({ email: '', password: '', autoSignIn: false });
+
+  const handleInputChange = (event) => {
+    setUserInfo({
+      ...userInfo,
+      [event.target.name]: event.target.value,
+    });
+  };
+
+  const handleCheckBoxClicked = () => {
+    setUserInfo({
+      ...userInfo,
+      autoSignIn: !userInfo.autoSignIn,
+    });
+  };
+
+  const signIn = () => {};
 
   return (
-    <Grid textAlign="center" style={{ height: '100vh' }} verticalAlign="middle">
-      <Grid.Column style={{ maxWidth: 450 }}>
+    <Grid className="signIn--grid"  verticalAlign="middle">
+      <Grid.Column className="signIn--grid__column">
         <Form size="large">
           <Segment stacked>
-            <Form.Input fluid icon="user" iconPosition="left" placeholder="E-mail address" />
-            <Form.Input fluid icon="lock" iconPosition="left" placeholder="Password" type="password" />
+            <Form.Input
+              value={userInfo.email}
+              onChange={handleInputChange}
+              name="email"
+              icon="user"
+              iconPosition="left"
+              placeholder="E-mail address"
+            />
+            <Form.Input
+              value={userInfo.password}
+              onChange={handleInputChange}
+              name="password"
+              icon="lock"
+              iconPosition="left"
+              placeholder="Password"
+              type="password"
+            />
+            <Form.Checkbox checked={userInfo.autoSignIn} onClick={handleCheckBoxClicked} label="자동로그인" />
             <Button color="teal" fluid size="large">
-              Login
+              로그인
             </Button>
           </Segment>
         </Form>
